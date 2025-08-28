c275ef485eb2e14de71ab632039f34d6f56f0177
ILYA BRASHAVETS <x3qop3@gmail.com>
2025-08-29 01:21:30 +0300

    Add files via upload

--- src/practicum/manager/Managers.java
+++ src/practicum/manager/Managers.java
@@ -1,10 +1,18 @@
-package practicum.manager;
+package manager;
+
+import java.io.File;
 
 public class Managers {
+
+    public static TaskManager getDefault() {
+        return new InMemoryTaskManager();
+    }
+
     public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
-    public static TaskManager getDefault() {
-        return new InMemoryTaskManager();
+
+    public static TaskManager getFileBacked(String name) {
+        return FileBackedTaskManager.loadFromFile(new File(name));
     }
 }
